99c4f35fc9f63f6c4d7cbef16864c9d4deaa06fe
Pradeep Ubale Patil <pradeepubale@users.noreply.github.com>
2022-09-27 15:49:54 +0530

    creating inline.cpp file

---
+++ 03inline.cpp
@@ -0,0 +1,36 @@
+// inline function
+
+#include<iostream>
+using namespace std;
+
+// use only when the code is small.
+// Don't use in recursion, or with static variable, when fun code is large.  
+inline int product(int a, int b){
+    return a * b;
+}
+
+int dot(int a, int b){
+    static int c = 0;
+    c += 1;
+    return (a*b)+c;
+}
+
+int main() {
+    int a,b;
+    cout<<"Enter value of a and b:";
+    cin>>a>>b;
+    cout<< "Product of a & b is: "<<product(a, b)<<endl;
+    cout<< "Product of a & b is: "<<product(a, b)<<endl;
+    cout<< "Product of a & b is: "<<product(a, b)<<endl;
+
+    //with static function
+    cout<< "Product of a & b is: "<<dot(a, b)<<endl;
+    cout<< "Product of a & b is: "<<dot(a, b)<<endl;
+    cout<< "Product of a & b is: "<<dot(a, b)<<endl;
+    return 0;
+}
+
+/*
+Run the program &
+attach program with output to your submission file.
+*/
