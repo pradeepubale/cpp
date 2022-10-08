2d11ee5ea90be1af3bb03c310e40acd7d77ab99a
Pradeep Ubale Patil <pradeepubale@users.noreply.github.com>
2022-10-08 15:25:58 +0530

    creating main program for custom header file.

---
+++ 06myheader.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include "demo.h"
+using namespace std;
+
+int main() {
+    hello();
+    int xa = myFunction(10, 20);
+    cout<<"Big no is "<<xa<<endl;
+    char str[] = "Hello";
+    Print("Hello c++");
+
+    dog d;
+    d.meow();
+    d.bho();
+    d.phurr();
+    return 0;
+}
+
+// Use above program as a Reference to solve Lab Assignment.
+
+/*
+Lab Assignment: 
+1. Create a header file with your own name. Put your personal information in functions.
+2. Import it in a program called "student.cpp"
+3. Create a class called "MyLife".(Either in main program OR in your header file).
+4. Call functions from header files in main()
+
+*/
